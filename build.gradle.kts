90378f43c3e2fe831155341e9556e6ee3f23a4ca
develop <SENSeiRIP@yandex.ru>
2023-04-16 15:48:04 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -6,14 +8,16 @@ val exposed_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.7.20"
+    id("io.ktor.plugin") version "2.2.4"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 group = "com.example"
-version = "0.0.1"
+version = "0.0.2"
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-
+    project.setProperty("mainClassName", mainClass.get())
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
@@ -21,6 +25,8 @@ application {
 repositories {
     mavenCentral()
 }
+
+val sshAntTask  = configurations.create("sshAntTask")
 
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
@@ -47,4 +53,74 @@ dependencies {
     implementation("junit:junit:4.12")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    sshAntTask("org.apache.ant:ant-jsch:1.10.12")
+}
+
+tasks.withType<ShadowJar> {
+    manifest {
+        attributes(
+            "Main-Class" to application.mainClass.get()
+        )
+    }
+}
+
+ant.withGroovyBuilder {
+    "taskdef"(
+        "name" to "scp",
+        "classname" to "org.apache.tools.ant.taskdefs.optional.ssh.Scp",
+        "classpath" to configurations.get("sshAntTask").asPath
+    )
+    "taskdef"(
+        "name" to "ssh",
+        "classname" to "org.apache.tools.ant.taskdefs.optional.ssh.SSHExec",
+        "classpath" to configurations.get("sshAntTask").asPath
+    )
+}
+
+task("deploy") {
+    dependsOn("clean", "shadowJar")
+    ant.withGroovyBuilder {
+        doLast {
+            val knownHosts = File.createTempFile("knownhosts", "txt")
+            val user = "root"
+            val host = "37.230.196.246"
+            val key = file("keys/ktorserverkey")
+            val jarFileName = "com.example.ktor-sample-all.jar"
+            try {
+                "scp"(
+                    "file" to file("build/libs/$jarFileName"),
+                    "todir" to "$user@$host:/root/test",
+                    "keyfile" to key,
+                    "trust" to true,
+                    "knownhosts" to knownHosts
+                )
+                "ssh"(
+                    "host" to host,
+                    "username" to user,
+                    "keyfile" to key,
+                    "trust" to true,
+                    "knownhosts" to knownHosts,
+                    "command" to "mv -v /root/test/$jarFileName /root/test/ktorserver.jar"
+                )
+                "ssh"(
+                    "host" to host,
+                    "username" to user,
+                    "keyfile" to key,
+                    "trust" to true,
+                    "knownhosts" to knownHosts,
+                    "command" to "systemctl stop ktortest"
+                )
+                "ssh"(
+                    "host" to host,
+                    "username" to user,
+                    "keyfile" to key,
+                    "trust" to true,
+                    "knownhosts" to knownHosts,
+                    "command" to "systemctl start ktortest"
+                )
+            } finally {
+                knownHosts.delete()
+            }
+        }
+    }
 }
